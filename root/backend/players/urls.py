fcbf47ec03d5ca22dc92ff73bc3591f5e307e226
Alex <acim650@gmail.com>
2023-09-20 09:51:09 -0700

    register user view function added

--- root/backend/players/urls.py
+++ root/backend/players/urls.py
@@ -1,16 +1,13 @@
 from django.urls import path
 from . import views
-from .views import home, homepage
 from django.views.generic import TemplateView
 
 
 urlpatterns = [
-    path('', views.homepage, name='home'),
-    path('login/', views.home, name='login'),
+    path('', views.home, name='home'),
     path('logout/', views.logout_user, name='logout'),
     path('register/', views.register_user, name='register'),
-    # path('records/', views.react_view, name='records'),
-    path('model-data/', views.get_model_data, name='model data'),
+    # path('model-data/', views.get_model_data, name='model data'),
 ]
 
 
--- root/backend/players/views.py
+++ root/backend/players/views.py
@@ -4,44 +4,38 @@ from django.contrib import messages
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.http import HttpResponse
-# from rest_framework import status
 from django.views.decorators.csrf import csrf_exempt
-from .forms import SignUpForm
+# from .forms import SignUpForm
 from .models import Record
 from .serializers import PlayerSerializer
 from django.http import JsonResponse
+from .forms import SignUpForm
 
 
-def homepage(request):
-    return render(request, 'home.html', {})
-
-@csrf_exempt 
+@csrf_exempt
 def home(request):
-    records = Record.objects.all()
+    # Check to see if logging in
     if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        response_data = {'message': 'Data received successfully'}
-        Response("You have been logged in successfully")
+        username = request.POST['username']
+        password = request.POST['password']
         # Authenticate
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
             messages.success(request, "You Have Been Logged In!")
-            return HttpResponse(response_data, status=status.HTTP_200_OK)
+            return redirect('home')
         else:
-            messages.success(request, "There was An Error Logging In, please try again...")
-            return HttpResponse("login not successful!")
+            messages.success(request, "There Was An Error Logging In, Please Try Again...")
+            return redirect('home')
     else:
-        return render(request, 'home.html', {'records': records})
+        return render(request, 'home.html', {})
 
 @csrf_exempt 
 def logout_user(request):
     logout(request)
     messages.success(request, "You Have Been Logged Out...")
-    return HttpResponse({ 'message': 'Logged out successfully'})
+    return redirect('home')
 
-@csrf_exempt 
 def register_user(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
@@ -52,52 +46,8 @@ def register_user(request):
             password = form.cleaned_data['password1']
             user = authenticate(username=username, password=password)
             login(request, user)
-            messages.success(request,"You Have Successfully Registered")
-            return HttpResponse("You're registered")
+            messages.success(request, "You have successfully registered")
+            return redirect('home')
     else:
         form = SignUpForm()
-        return HttpResponse("You're not registered")
-    
-    return render(request, 'homeLayout.tsx', {'form': form})
-
-@csrf_exempt 
-def get_model_data(request):
-    if request.method == 'GET':
-        model_data = Record.objects.all()
-        serializer = PlayerSerializer(model_data, many=True)
-        formatted_data = [
-            {
-                'created_at': player['created_at'],
-                'first_name': player['first_name'],
-                'last_name': player['last_name'],
-                'email': player['email'],
-                'phone': player['phone']
-            }
-            for player in serializer.data
-        ]
-        # return HttpResponse(formatted_data)
-        return JsonResponse(formatted_data, safe=False)
-    else:
-        return
-
-def customer_record(request, pk):
-     if request.user.is_authenticated:
-        #   Look up Records
-        customer_record = Record.objects.get(id=pk)
-        return render(request, 'home.html', {'customer_record': customer_record})
-     else:
-        messages.success(request, "You must be logged in to view that page...")
-        return redirect('home')
-
-
-@api_view(['POST'])
-def delete_record(request, pk):
-    if request.user.is_authenticated:
-        delete_it = Record.objects.get(id=pk)
-        delete_it.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-    else:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-
-
+        return render(request, 'register.html', {'form': form})
